5f29b642d09bdb31dd40b77cc87e2f9e22b48768
Larissa Camyle <larissacamylejob@gmail.com>
2024-10-10 18:02:23 -0300

    alterações no arquivo scss criando variáveis

---
+++ js/eventosCSS.js
@@ -0,0 +1,15 @@
+const btnTemaClick = 0;
+
+const eventosCss={
+    trocaTemaFuncao (){
+        btnTemaClick =+ 1;
+
+        
+
+    }
+
+
+
+}
+
+export default eventosCss;
---
+++ js/script.js
@@ -0,0 +1,5 @@
+import eventosCss from '/js/eventosCSS.js'
+
+//EVENTOS CSS
+const trocarTema = document.querySelector("header button");
+trocarTema.addEventListener("click", eventosCss.trocaTemaFuncao)
