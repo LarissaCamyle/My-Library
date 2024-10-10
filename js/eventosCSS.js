42332efad699cce8e02a6dbf4e67ab12bf38e227
Larissa Camyle <larissacamylejob@gmail.com>
2024-10-10 19:19:25 -0300

    alterações no js

--- js/eventosCSS.js
+++ js/eventosCSS.js
@@ -1,15 +1,52 @@
-const btnTemaClick = 0;
+let btnTemaClick = 0;
+// Seleciona o elemento :root
+const root = document.documentElement;
 
 const eventosCss={
-    trocaTemaFuncao (){
-        btnTemaClick =+ 1;
+    trocaTemaFuncao (event){
+        event.preventDefault();
+        btnTemaClick += 1;
 
-        
+        if(btnTemaClick === 1){
+            root.style.setProperty('--destaque1', '#C69EFF');
+            root.style.setProperty('--destaque2', '#6B44A3');
+        }
+        if(btnTemaClick === 2){
+            root.style.setProperty('--destaque1', '#9DD8D3');
+            root.style.setProperty('--destaque2', '#136375');
+        }
+        if(btnTemaClick === 3){
+            root.style.setProperty('--destaque1', '#FADDD3');
+            root.style.setProperty('--destaque2', '#DD713C');
+        }
+        if(btnTemaClick === 4){
+            root.style.setProperty('--destaque1', '#AAD89D');
+            root.style.setProperty('--destaque2', '#CF4257');
+            btnTemaClick = 0;
+        }
+    },
 
+    abrirFormulario(event){
+        event.preventDefault();
+        const btnAdicionarAbrirForm = document.querySelector(".adicionar-livro")
+        btnAdicionarAbrirForm.classList.toggle("hidden");
+
+        const form = document.querySelector("form");
+        form.classList.toggle("hidden")
+    },
+
+    xFormulario (event) {
+        event.preventDefault();
+        const form = document.querySelector("form");
+        form.classList.toggle("hidden");
+    },
+
+    preencherEstrelasForm(event, btnClicado){
+        alert("b")
     }
 
 
 
-}
+};
 
 export default eventosCss;
--- js/script.js
+++ js/script.js
@@ -2,4 +2,9 @@ import eventosCss from '/js/eventosCSS.js'
 
 //EVENTOS CSS
 const trocarTema = document.querySelector("header button");
-trocarTema.addEventListener("click", eventosCss.trocaTemaFuncao)
+trocarTema.addEventListener("click", (evento) => eventosCss.trocaTemaFuncao(evento));
+const btnAdicionarAbrirForm = document.querySelector(".adicionar-livro");
+btnAdicionarAbrirForm.addEventListener("click", (evento) => eventosCss.abrirFormulario(evento));
+const btnXForm = document.querySelector(".fechar");
+btnXForm.addEventListener("click", (evento) => eventosCss.xFormulario(evento));
+
